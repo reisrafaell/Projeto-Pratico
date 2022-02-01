f1ab245a5c0aa3de8ef4e026be7d49547cb0ecc5
Rafael Reis <rafaeloliveira63@yahoo.com>
2022-02-01 20:16:16 -0300

    Feat: modificando a estrutura do código

--- login.js
+++ login.js
@@ -1,3 +1,18 @@
 export default function login(){
+
+
+    const butoon = document.getElementById('#buttonLogin')
+
+    const user = document.getElementById('#user')
+
+    const senha = document.getElementById('#senhaa')
+
+    const usuario = user.textContent
+    const Senha = senha.textContent
+
+    butoon.addEventListener('click',() =>{
+        window.alert('ok')
     
+    })
 }
+
